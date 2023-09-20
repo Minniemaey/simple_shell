99c1f608d9ef7404c1aad292cca712e95091197f
Minniemaey <ayodedeyi@gmail.com>
2023-09-20 12:35:25 -0700

    updated shell.h

--- shell.h
+++ shell.h
@@ -15,8 +15,18 @@
 
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
 #define MAX_BUFFER_SIZE 1024
 #define MAX_ARGUMENTS 100
+
+#define USE_GETLINE 0
+#define USE_STRKOK 0
+
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
 
 extern char **environ;
 
@@ -67,6 +77,53 @@ typedef struct datast
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
-int _getline(char **ptr, size_t *n, FILE *str);
+/**
+ * struct liststr - a singly linked list
+ * @num: number
+ * @str: string
+ * @next: pointer to next node
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/*data struct prototypes*/
+void init_dat(data_t *data, char **av);
+void free_dat(data_t *data, int n);
+void clr_dat(data_t *data);
+
+/*write functions*/
+int _putchar(char c);
+int _putchar_fd(char c, int fd);
+int _puts_fd(char *s, int fd);
+
+/*read line functions*/
+int _getline(data_t *data, char **ptr, size_t *size);
+ssize_t rd_buff(data_t *data, char *buff, size_t *s);
+ssize_t _getbuff(data_t *data, char **buff, size_t *len);
+ssize_t rd_line(data_t *data);
+void _sigint(__attribute__((unused))int sig);
+
+/*error handling functions*/
+int _putchar_err(char c);
+void _puts_err(char *s);
+
+/*command execution functions*/
+void _cmdfork(data_t *data);
+
+/*custom string functions*/
+int _strlen(char *ptr);
+char *_strchr(char *s, char c);
+char *_strcat(char *dest, char *src);
+char *_str_concat(char *src, char *dest, int n);
+int _strcmp(char *str1, char *str2);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *s);
+
+
+
 
 #endif
